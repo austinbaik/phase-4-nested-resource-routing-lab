a253dc18d07d4db34f3cf36dad1f11fc8b721756
Austin <baik.austin@gmail.com>
2023-01-31 19:33:43 -0800

    commit tree 52a24c7b0e4e32dc6d994eed89fd186e74ddd13e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,45 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      # render_not_found_response
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  # there is a user_id 
+  # there isn't a user_id and only /items
+
+  def show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "user not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    # nested resource
+    resources :items, only: [:show, :index, :create]
+  end
 end
+
+
+
+
+
+
+
+
